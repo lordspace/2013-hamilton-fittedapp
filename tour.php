a589818a0a5b931a4259623ff6af54ce442de357
Svetoslav Marinov (Slavi) <slavi@orbisius.com>
2013-04-27 15:10:40 -0400

    aaaaa

---
+++ tour.php
@@ -0,0 +1,9 @@
+<?php require_once dirname(__FILE__) . '/header.php'; ?>
+
+<div class="row-fluid app_container">
+    <p>
+    Tour.
+    </p>
+</div>
+
+<?php require_once dirname(__FILE__) . '/footer.php'; ?>
