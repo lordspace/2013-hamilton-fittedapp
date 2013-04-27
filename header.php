e1e9c18c6842f5955d3c4012df2060bae7ab57a7
Svetoslav Marinov (Slavi) <slavi@orbisius.com>
2013-04-27 16:48:21 -0400

    added logo

--- header.php
+++ header.php
@@ -40,7 +40,7 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="brand" href="#">Fitted</a>
+            <a class="brand" href="index.php"><img src="assets/logo.png">Fitted</a></a>
             <!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
             <div class="nav-collapse collapse">
               <ul class="nav">
@@ -52,7 +52,7 @@
                     <a href="join.php">Login / Join</a>
                 </li>
                 <!-- Read about Bootstrap dropdowns at http://twitter.github.com/bootstrap/javascript.html#dropdowns -->
-                <li class="dropdown">
+                <!--<li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
                   <ul class="dropdown-menu">
                     <li><a href="#">Action</a></li>
@@ -63,7 +63,7 @@
                     <li><a href="#">Separated link</a></li>
                     <li><a href="#">One more separated link</a></li>
                   </ul>
-                </li>
+                </li>-->
               </ul>
             </div><!--/.nav-collapse -->
           </div><!-- /.navbar-inner -->
