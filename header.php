2190dbefd4220de0c2c1f0e46dbb6aad87a84f55
Svetoslav Marinov (Slavi) <slavi@orbisius.com>
2013-04-27 15:33:24 -0400

    changed top menu

--- header.php
+++ header.php
@@ -10,35 +10,52 @@
     </head>
     <body>
         <div class="container-fluid">
-            <div class="navbar navbar-inverse navbar-fixed-top">
-                <div class="navbar-inner">
-                    <div class="container">
-                        <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
 
-                        <a class="brand" href="index.php">Home</a>
-                        <div class="nav-collapse collapse">
-                            <ul class="nav">
-                                <li class="app_call_to_action">
-                                    <a href="try.php">Try It Now!</a>
-                                </li>
-                                <li class="">
-                                    <a href="contact.php">Contact</a>
-                                </li>
-                                <li class="">
-                                    <a href="about.php">About</a>
-                                </li>
-                                <li class="active">
-                                    <a href="join.php">Login / Join</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <!-- NAVBAR
+    ================================================== -->
+    <div class="navbar-wrapper">
+      <!-- Wrap the .navbar in .container to center it within the absolutely positioned parent. -->
+      <div class="container">
+
+        <div class="navbar navbar-inverse">
+          <div class="navbar-inner">
+            <!-- Responsive Navbar Part 1: Button for triggering responsive navbar (not covered in tutorial). Include responsive CSS to utilize. -->
+            <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="brand" href="#">Fitted</a>
+            <!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
+            <div class="nav-collapse collapse">
+              <ul class="nav">
+                <li class=""><a href="index.php">Home</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="about.php">About</a></li>
+                <li class="active">
+                    <a href="join.php">Login / Join</a>
+                </li>
+                <!-- Read about Bootstrap dropdowns at http://twitter.github.com/bootstrap/javascript.html#dropdowns -->
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li class="divider"></li>
+                    <li class="nav-header">Nav header</li>
+                    <li><a href="#">Separated link</a></li>
+                    <li><a href="#">One more separated link</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div><!--/.nav-collapse -->
+          </div><!-- /.navbar-inner -->
+        </div><!-- /.navbar -->
+
+      </div> <!-- /.container -->
+    </div><!-- /.navbar-wrapper -->
+
 
             <div class="row-fluid">
                 <p>&nbsp;</p>
--- join.php
+++ join.php
@@ -1,16 +1,16 @@
 <?php require_once dirname(__FILE__) . '/header.php'; ?>
 
-<div class="row-fluid app_container">
-    <div class="span6 app_vertical_separator app_center">
+<div class="row-fluid app_container ">
+    <div class="span6 hero-unit">
         <h2>Login</h2>
         <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn btn-primary" href="#">Login</a></p>
+        <p><a class="btn-large btn-primary" href="#">Login</a></p>
     </div><!--/span-->
 
-    <div class="span6">
+    <div class="span6 hero-unit">
         <h2>Sign Up!</h2>
         <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn btn-primary" href="#">Sign Up</a></p>
+        <p><a class="btn-large btn-primary" href="#">Sign Up</a></p>
     </div><!--/span-->
 </div>
 
