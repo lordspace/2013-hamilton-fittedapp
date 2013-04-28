fc75e037b95844c4ab332be9d01915e3db3309ff
Svetoslav Marinov (Slavi) <slavi@orbisius.com>
2013-04-28 11:53:53 -0400

    updated info on the home page

--- index.php
+++ index.php
@@ -1,30 +1,26 @@
 <?php require_once dirname(__FILE__) . '/header.php'; ?>
 
 <div class="row-fluid">
-    <div class="span12">
-        <div class="hero-unit">
-            <h1>What is Fitted?</h1>
-            <p>Fitted takes the sizes of the clothes that fit you best and lets you know how you size up in all your other favourite brands!</p>
-            <p><a href="try.php" class="btn btn-primary btn-large">Try It Now! &raquo;</a></p>
-        </div>
-    </div> <!-- /row-fluid -->
+    <div class="hero-unit">
+        <h1>What is Fitted?</h1>
+        <p>Fitted takes the sizes of the clothes that fit you best and lets you know how you size up in all your other favourite brands!</p>
+        <p><a href="try.php" class="btn btn-primary btn-large">Try It Now! &raquo;</a></p>
+    </div>
 </div> <!-- /row-fluid -->
 
 <div class="row-fluid">
     <div class="span4">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn" href="#">View details &raquo;</a></p>
-    </div><!--/span-->
-    <div class="span4">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn" href="#">View details &raquo;</a></p>
-    </div><!--/span-->
-    <div class="span4">
         <h2>Shop With Confidence</h2>
         <p>Use our recommendation engine to find and buy clothes that really fit you.</p>
         <!--<p><a class="btn btn-primary" href="#">View details &raquo;</a></p>-->
+    </div><!--/span-->
+    <div class="span4">
+        <h2>Big Savings!</h2>
+        <p>Get alerts when the brands you love go on sale.</p>
+    </div><!--/span-->
+    <div class="span4">
+        <h2>Wishlists</h2>
+        <p>Share with your friends whatever you like.</p>
     </div><!--/span-->
 </div><!--/row-->
 
