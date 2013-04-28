97ee19fd2345ec937f361cd7d0b344a535e3d114
Svetoslav Marinov (Slavi) <slavi@orbisius.com>
2013-04-28 11:24:15 -0400

    updated try to ask if the person knows their size or want to give measurments.

--- try.php
+++ try.php
@@ -9,89 +9,94 @@ $default_data = array(
     'clothing_type' => 'shirt',
 );
 
+$step = empty($_REQUEST['step']) ? 0 : intval($_REQUEST['step']);
 $data = empty($_REQUEST['data']) ? array() : $_REQUEST['data'];
 $data = array_map('trim', $data);
 $data = array_merge($default_data, $data);
 
 ?>
 
-<!--Do you know your size well enough to make a purchase?
-Yes, I know which size to buy
-No, I'm not sure which size fits me best
--->
-
 <form id="qform" method="POST">
+    <input type="hidden" name="step" value="<?php echo $step; ?>" />
     <div class="row-fluid app_container">
-        <h2>I know my size</h2>
-        <div class="app_user_question">
-            <label for="app-q1">What brand is it?</label>
-            <input type="text" id="app-q1" name="data[brand]" value="<?php echo $data['brand']; ?>"/>
-        </div>
-
-        <div class="app_user_question">
-            <label for="app-q2">What size is it?</label>
-            <input type="text" id="app-q2" name="data[size]" value="<?php echo $data['size']; ?>" />
-        </div>
-
-        <div class="app_user_question">
-            <label for="app-q3">What style is it?</label>
-            <input type="text" id="app-q3" name="data[style]" value="<?php echo $data['style']; ?>" />
-        </div>
-
-        <div class="app_user_question">
-            <label for="q4_1">What item of clothing is it?</label>
-
-            <div id="radio">
-                <label for="q4_1"><input type="radio" name="data[clothing_type]" value="shirt" id="q4_1" 
-                    <?php echo $data['clothing_type'] == 'shirt' ? 'checked="checked"' : ''; ?> /> <span>Shirt</span></label>
-                <!--<label for="q4_2"><input type="radio" name="data[clothing_type]" value="dress" id="q4_2" readonly="readonly" /> <span>Dress</span></label>
-                <label for="q4_3"><input type="radio" name="data[clothing_type]" value="tops" id="q4_3" readonly="readonly" /> <span>Tops</span></label>
-                -->
+        <?php if ($step == 1) : ?>
+            <h2>I know my size</h2>
+            <div class="app_user_question">
+                <label for="app-q1">What brand is it?</label>
+                <input type="text" id="app-q1" name="data[brand]" value="<?php echo $data['brand']; ?>"/>
             </div>
-        </div>
 
-        <br class="clear_both" />
-        <input type="submit" class="btn btn-primary" value="Submit" />
-        <br class="clear_both"/>
-        
-        <h2>I want to know my size by entering my dimensions (inches).</h2>
-        <div class="app_question">
-            <label for="size-height">Your Height:</label>
-            <input type="text" id="size-height" />
+            <div class="app_user_question">
+                <label for="app-q2">What size is it?</label>
+                <input type="text" id="app-q2" name="data[size]" value="<?php echo $data['size']; ?>" />
+            </div>
 
-            <div id="height-slider" class="slider"></div>
-        </div>
+            <div class="app_user_question">
+                <label for="app-q3">What style is it?</label>
+                <input type="text" id="app-q3" name="data[style]" value="<?php echo $data['style']; ?>" />
+            </div>
 
-        <div class="app_question">
-            <label for="size-bust">Your Bust Size:</label>
-            <input type="text" id="size-bust" />
+            <div class="app_user_question">
+                <label for="q4_1">What item of clothing is it?</label>
 
-            <div id="slider-bust" class="slider"></div>
-        </div>
+                <div id="radio">
+                    <label for="q4_1"><input type="radio" name="data[clothing_type]" value="shirt" id="q4_1"
+                        <?php echo $data['clothing_type'] == 'shirt' ? 'checked="checked"' : ''; ?> /> <span>Shirt</span></label>
+                    <!--<label for="q4_2"><input type="radio" name="data[clothing_type]" value="dress" id="q4_2" readonly="readonly" /> <span>Dress</span></label>
+                    <label for="q4_3"><input type="radio" name="data[clothing_type]" value="tops" id="q4_3" readonly="readonly" /> <span>Tops</span></label>
+                    -->
+                </div>
+            </div>
 
-        <div class="app_question">
-            <label for="size-waist">Your Waist Size:</label>
-            <input type="text" id="size-waist" />
+            <br class="clear_both" />
+            <input type="submit" class="btn btn-primary" value="Submit" />
+        <?php elseif ($step == 2) : ?>
+            <br class="clear_both"/>
+            <h2>I want to know my size by entering my dimensions (inches).</h2>
+            <div class="app_question">
+                <label for="size-height">Your Height:</label>
+                <input type="text" id="size-height" />
 
-            <div id="slider-waist" class="slider"></div>
-        </div>
+                <div id="height-slider" class="slider"></div>
+            </div>
 
-        <div class="app_question">
-            <label for="size-waist">Your Hips Size:</label>
-            <input type="text" id="size-hips" />
+            <div class="app_question">
+                <label for="size-bust">Your Bust Size:</label>
+                <input type="text" id="size-bust" />
 
-            <div id="slider-hips" class="slider"></div>
-        </div>
+                <div id="slider-bust" class="slider"></div>
+            </div>
 
-        <div class="app_question">
-            <label for="size-arm">Your Arm Size:</label>
-            <input type="text" id="size-arm" />
+            <div class="app_question">
+                <label for="size-waist">Your Waist Size:</label>
+                <input type="text" id="size-waist" />
 
-            <div id="slider-arm" class="slider"></div>
-        </div>
+                <div id="slider-waist" class="slider"></div>
+            </div>
 
-        <br class="clear_both"/>
-        <input type="submit" class="btn btn-primary" value="Submit" />
+            <div class="app_question">
+                <label for="size-waist">Your Hips Size:</label>
+                <input type="text" id="size-hips" />
+
+                <div id="slider-hips" class="slider"></div>
+            </div>
+
+            <div class="app_question">
+                <label for="size-arm">Your Arm Size:</label>
+                <input type="text" id="size-arm" />
+
+                <div id="slider-arm" class="slider"></div>
+            </div>
+
+            <br class="clear_both"/>
+            <input type="submit" class="btn btn-primary" value="Submit" />
+        <?php else : ?>
+            <div class="app_user_question0 hero-unit">
+                <h2>Do you know your size?</h2>
+                <a href="try.php?step=1" class="btn btn-primary">Yes, I know my size</a>
+                <a href="try.php?step=2" class="btn btn-primary">No, I want to give my measurements</a>
+            </div>
+        <?php endif; ?>
     </div>
 </form>
 
